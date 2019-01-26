4aa1a37bed230906f9dfe08ddf753a3e798f28be
theahmetcanbey <46626902+theahmetcanbey@users.noreply.github.com>
2019-01-26 19:44:05 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -94,9 +94,9 @@ rootContent = new Vue({
 			}else alert("İsim değiştirilmedi, eski isim ile yeni isim aynı görünüyor.");
 		},
 		getHi: function (a){
-			console.log(a);
+			//console.log(a);
 			a *= 1;
-			console.log(a);
+			//console.log(a);
 			return date("H:i", a);
 		}
 	}
